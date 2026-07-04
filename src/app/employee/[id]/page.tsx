d90b985db59e47898f54977cab1b47d7d456e98f
Viktor Baltes <viktorbaltes@Viktor-MacBook-Air.local>
2026-07-04 20:37:32 +0200

    Add Svida logo to evaluation form

--- src/app/employee/[id]/page.tsx
+++ src/app/employee/[id]/page.tsx
@@ -38,10 +38,18 @@ export default async function EmployeePage({ params }: Props) {
   }
 
   return (
-    <main className="max-w-3xl mx-auto p-8">
-      <h1 className="text-4xl font-bold">
-        {employee.first_name} {employee.last_name}
-      </h1>
+  <main className="max-w-3xl mx-auto p-8">
+    <div className="mb-8 flex justify-center">
+      <img
+        src="/logo-svida.png"
+        alt="Senior dom Svida"
+        className="h-24 w-auto"
+      />
+    </div>
+
+    <h1 className="text-4xl font-bold">
+      {employee.first_name} {employee.last_name}
+    </h1>
 
       <p className="text-gray-500 mt-2">
         {(employee.departments as any)?.name}
